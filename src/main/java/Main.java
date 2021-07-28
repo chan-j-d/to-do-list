864d2ea3982045e850e9b8a67ab88439a9112adf
chan-j-d <chan.j.d.98@gmail.com>
2021-07-28 19:44:04 +0800

    Use Parse class in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,24 +1,32 @@
+import parser.ParseException;
+import parser.Parser;
 import task.TaskList;
+import util.Pair;
 
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    private static final String MESSAGE_ERROR_HEADER = "Error: ";
 
+    public static void main(String[] args) {
         TaskList taskList = new TaskList();
 
         Scanner scanner = new Scanner(System.in);
         scanner.useDelimiter("\n");
         String input = scanner.next();
+        Parser parser = new Parser();
         while (!input.equals("exit")) {
-            taskList.addTask(day, taskString);
-            System.out.println(taskList);
+            try {
+                Pair<String, String> inputPair = parser.parse(input);
+                taskList.addTask(inputPair.getFirst(), inputPair.getSecond());
+                System.out.println(taskList);
+            } catch (ParseException pe){
+                System.err.println(MESSAGE_ERROR_HEADER + pe.getMessage());
+            }
             input = scanner.next();
         }
-
-
     }
 
 }
