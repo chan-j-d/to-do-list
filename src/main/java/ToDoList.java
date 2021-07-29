c674a1e29262290c95a0609e13b3134220807626
chan-j-d <chan.j.d.98@gmail.com>
2021-07-29 22:49:20 +0800

    Fix minor code quality

--- src/main/java/ToDoList.java
+++ src/main/java/ToDoList.java
@@ -2,24 +2,22 @@ import parser.ParseException;
 import parser.Parser;
 import storage.JsonStorageImpl;
 import storage.Storage;
-import task.Task;
 import task.TaskList;
 import util.Pair;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.Optional;
 import java.util.Scanner;
 
 public class ToDoList {
 
-    private static final String SAVEFILE_NAME = "todolist-save";
-    private static final Path DEFAULT_PATH = Paths.get(SAVEFILE_NAME);
+    private static final String SAVE_FILE_NAME = "todolist-save";
+    private static final Path DEFAULT_PATH = Paths.get(SAVE_FILE_NAME);
 
     private final Storage storage;
     private final Parser parser;
+    private final TaskList taskList;
     private Path saveDirectory;
-    private TaskList taskList;
 
     public ToDoList() {
         storage = new JsonStorageImpl();
