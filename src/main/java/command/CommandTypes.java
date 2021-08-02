030e9027345747d9c1df674c3e82d6f6d6f6d173
chan-j-d <chan.j.d.98@gmail.com>
2021-08-02 18:23:36 +0800

    Create edit task command class

--- src/main/java/command/CommandTypes.java
+++ src/main/java/command/CommandTypes.java
@@ -8,7 +8,9 @@ public class CommandTypes {
     public static final String COMMAND_DELETE = "delete";
     public static final String COMMAND_COMPLETE = "do";
     public static final String COMMAND_UNDO = "undo";
+    public static final String COMMAND_EDIT = "edit";
 
-    public static List<String> COMMAND_TYPES = List.of(COMMAND_ADD, COMMAND_COMPLETE, COMMAND_DELETE, COMMAND_UNDO);
+    public static List<String> COMMAND_TYPES = List.of(COMMAND_ADD, COMMAND_COMPLETE, COMMAND_DELETE,
+            COMMAND_UNDO, COMMAND_EDIT);
 
 }
