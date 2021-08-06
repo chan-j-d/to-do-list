a25bc4f340e53f9db84790859149d8a8feaa13b9
chan-j-d <chan.j.d.98@gmail.com>
2021-08-07 02:13:44 +0800

    add text field to block

--- src/main/java/gui/AddTaskBlockGui.java
+++ src/main/java/gui/AddTaskBlockGui.java
@@ -1,13 +1,26 @@
 package gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 
 public class AddTaskBlockGui extends GuiComponent<HBox> {
 
     private static final String FXML_RESOURCE = "AddTaskBlock.fxml";
 
+    @FXML
+    private HBox block;
+
+    private final TextField textField;
+
     public AddTaskBlockGui() {
         super(FXML_RESOURCE);
+        textField = new TextField();
+    }
+
+    @FXML
+    private void createTextField() {
+        block.getChildren().add(textField);
     }
 
 }
