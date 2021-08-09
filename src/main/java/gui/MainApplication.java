320b578497c083dc4809e1e7ffdf5360c830c3c6
chan-j-d <chan.j.d.98@gmail.com>
2021-08-09 18:37:32 +0800

    Add exit command to terminate asynchronous process

--- src/main/java/gui/MainApplication.java
+++ src/main/java/gui/MainApplication.java
@@ -1,5 +1,6 @@
 package gui;
 
+import command.ExitCommand;
 import core.ToDoList;
 import io.IOInterface;
 import io.gui.GuiIO;
@@ -11,18 +12,25 @@ import java.util.concurrent.CompletableFuture;
 
 public class MainApplication extends Application {
 
+    private MainWindow mainWindow;
+
      @Override
      public void start(Stage stage) {
-         MainWindow mainWindow = new MainWindow();
+         mainWindow = new MainWindow();
          IOInterface ioInterface = new GuiIO(mainWindow);
          ToDoList toDoList = new ToDoList(ioInterface);
-         CompletableFuture<Void> future = CompletableFuture.runAsync(() -> toDoList.run());
+         CompletableFuture.runAsync(toDoList::run);
          Scene scene = new Scene(mainWindow.getRoot());
          stage.setScene(scene);
          stage.setTitle("To-Do-List");
          stage.setMinHeight(400.0);
          stage.setMinWidth(400.0);
          stage.show();
+     }
+
+     @Override
+     public void stop() {
+         mainWindow.runUserCommand(new ExitCommand());
      }
 
      public static void main(String[] args) {
