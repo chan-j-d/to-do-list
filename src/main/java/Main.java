897378dbac4bc9f56b225a2e2573368a27987693
chan-j-d <chan.j.d.98@gmail.com>
2021-08-09 22:28:44 +0800

    Move main method to Main class

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import gui.MainApplication;
+import javafx.application.Application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Application.launch(MainApplication.class, args);
+    }
+}
