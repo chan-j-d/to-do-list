47b9b703960d84cf41f5a13052efc56f348a2656
chan-j-d <chan.j.d.98@gmail.com>
2021-08-11 16:44:58 +0800

    Update test case to include existent and nonexistent directories

--- src/test/java/storage/JsonStorageImplTest.java
+++ src/test/java/storage/JsonStorageImplTest.java
@@ -12,18 +12,28 @@ import java.util.Optional;
 
 public class JsonStorageImplTest {
 
-    private static final Path TEST_FILE_PATH = Paths.get("build/test-save");
+    private static final Path TEST_FILE_PATH_NO_DIRECTORY = Paths.get("build/test-save-directory/test-save");
+    private static final Path TEST_FILE_PATH_WITH_DIRECTORY = Paths.get("build/test-save");
 
     private final Storage storage = new JsonStorageImpl();
 
     @Test
     public void saveAndLost_normalTaskList_success() {
         TaskList expectedList = TaskListTemplate.buildTaskListTemplate();
-        storage.save(expectedList, TEST_FILE_PATH);
+        // directory already exists
+        storage.save(expectedList, TEST_FILE_PATH_WITH_DIRECTORY);
 
-        Optional<TaskList> optionalTaskList = storage.load(TEST_FILE_PATH);
+        Optional<TaskList> optionalTaskList = storage.load(TEST_FILE_PATH_WITH_DIRECTORY);
         Assertions.assertTrue(optionalTaskList.isPresent());
         Assertions.assertEquals(expectedList, optionalTaskList.get());
-        FileUtil.deleteFile(TEST_FILE_PATH);
+        FileUtil.deleteFile(TEST_FILE_PATH_WITH_DIRECTORY);
+
+        //directory does not already exist
+        storage.save(expectedList, TEST_FILE_PATH_NO_DIRECTORY);
+
+        optionalTaskList = storage.load(TEST_FILE_PATH_NO_DIRECTORY);
+        Assertions.assertTrue(optionalTaskList.isPresent());
+        Assertions.assertEquals(expectedList, optionalTaskList.get());
+        FileUtil.deleteFile(TEST_FILE_PATH_NO_DIRECTORY);
     }
 }
