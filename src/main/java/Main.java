fadfaf6bca6f0d3c7a996e7dd94b5eed4d480e58
chan-j-d <chan.j.d.98@gmail.com>
2021-08-13 20:34:33 +0800

    Add logging for javafx launch failure

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,20 @@
 import gui.MainApplication;
 import javafx.application.Application;
+import logging.LogsManager;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main {
 
+    private static final Logger LOGGER = LogsManager.getLogger(Main.class);
+    private static final String ERROR_LAUNCHING_GUI = "Error launching gui for application.";
+
     public static void main(String[] args) {
-        Application.launch(MainApplication.class, args);
+        try {
+            Application.launch(MainApplication.class, args);
+        } catch (RuntimeException re) {
+            LOGGER.log(Level.SEVERE, ERROR_LAUNCHING_GUI,re);
+        }
     }
 }
--- src/main/java/logging/LogsManager.java
+++ src/main/java/logging/LogsManager.java
@@ -1,6 +1,8 @@
 package logging;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.FileHandler;
 import java.util.logging.Formatter;
 import java.util.logging.Handler;
@@ -11,6 +13,7 @@ public class LogsManager {
     private static final long LIMIT = (1 << 20);
     private static final int FILE_COUNT = 1;
     private static final Formatter FORMATTER = new LogFormatter();
+    private static final List<Logger> LOGGERS = new ArrayList<>();
 
     private static Handler FILE_HANDLER;
 
@@ -19,6 +22,7 @@ public class LogsManager {
         logger.setUseParentHandlers(false);
 
         logger.addHandler(FILE_HANDLER);
+        LOGGERS.add(logger);
         return logger;
     }
 
@@ -32,5 +36,6 @@ public class LogsManager {
                 FILE_COUNT,
                 true);
         FILE_HANDLER.setFormatter(FORMATTER);
+        LOGGERS.forEach(logger -> logger.addHandler(FILE_HANDLER));
     }
 }
