36183b0fee2fd2524e1b3dca0b3401b17d624a1c
chan-j-d <chan.j.d.98@gmail.com>
2021-08-16 21:13:35 +0800

    Implement changes to JsonTaskList

--- src/main/java/storage/JsonTaskList.java
+++ src/main/java/storage/JsonTaskList.java
@@ -1,9 +1,10 @@
 package storage;
 
-import static task.BlockNames.BLOCK_NAMES;
-
 import com.fasterxml.jackson.annotation.JsonProperty;
-import java.util.LinkedHashMap;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import task.TaskBlock;
@@ -12,32 +13,32 @@ import task.TaskList;
 public class JsonTaskList {
 
     private final Map<String, JsonTaskBlock> blocks;
-
+    private final List<String> keyOrder;
 
     /**
      * Creates a JSON equivalent object of a {@code TaskList} for saving.
      */
-    public JsonTaskList(@JsonProperty("blocks") Map<String, JsonTaskBlock> blocks) {
-        this.blocks = new LinkedHashMap<>();
-        for (String key : blocks.keySet()) {
-            this.blocks.put(key, blocks.get(key));
-        }
+    public JsonTaskList(@JsonProperty("keyOrder") List<String> keyOrder,
+            @JsonProperty("blocks") Map<String, JsonTaskBlock> blocks) {
+        this.keyOrder = new ArrayList<>();
+        this.keyOrder.addAll(keyOrder);
+
+        this.blocks = new HashMap<>();
+        this.blocks.putAll(blocks);
     }
 
-    /**
-     * Returns the Java equivalent {@code TaskList} of itself.
-     */
     public TaskList toJavaType() {
-        Map<String, TaskBlock> nonJsonBlocks = new LinkedHashMap<>();
-        for (String day : BLOCK_NAMES) {
-            nonJsonBlocks.put(day, blocks.get(day).toJavaType());
-        }
-        return new TaskList(nonJsonBlocks);
+        Map<String, TaskBlock> map = blocks.entrySet()
+                .stream()
+                .collect(Collectors.toMap(Map.Entry::getKey, entry -> entry.getValue().toJavaType()));
+        return new TaskList(keyOrder, map);
     }
 
     public static JsonTaskList convertToJson(TaskList taskList) {
-        return new JsonTaskList(taskList.getBlocksMap().entrySet().stream()
-                .collect(Collectors.toMap(Map.Entry::getKey, entry -> JsonTaskBlock.convertToJson(entry.getValue()))));
+        return new JsonTaskList(taskList.getKeyOrder(),
+                taskList.getBlocksMap().entrySet().stream()
+                        .collect(Collectors.toMap(Map.Entry::getKey,
+                                entry -> JsonTaskBlock.convertToJson(entry.getValue()))));
     }
 
     @Override
