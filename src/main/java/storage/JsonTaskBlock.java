22e7222521526523142f3c94dff30986a37ec67e
chan-j-d <65345505+chan-j-d@users.noreply.github.com>
2021-08-16 21:05:29 +0800

    Add Json storer interface (#46)
    Adds a JsonStorer interface
    
    Serves as a contract for storage classes
    to implement the toJavaType method.

--- src/main/java/storage/JsonTaskBlock.java
+++ src/main/java/storage/JsonTaskBlock.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import task.TaskBlock;
 
-public class JsonTaskBlock {
+public class JsonTaskBlock implements JsonStorer<TaskBlock> {
 
     private final String blockHeader;
     private final List<JsonTask> tasks;
