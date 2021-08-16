053ee3e2b89067e0d773418b8ee5602cf4bbe83f
chan-j-d <chan.j.d.98@gmail.com>
2021-08-16 18:10:50 +0800

    Remove redundant method

--- src/main/java/task/BlockNames.java
+++ src/main/java/task/BlockNames.java
@@ -4,10 +4,9 @@ import java.util.List;
 
 public class BlockNames {
 
+    /**
+     * Standard list of block names.
+     */
     public static final List<String> BLOCK_NAMES = List.of("monday", "tuesday", "wednesday", "thursday",
             "friday", "saturday", "sunday");
-
-    public static boolean isValidBlockName(String blockName) {
-        return BLOCK_NAMES.contains(blockName);
-    }
 }
