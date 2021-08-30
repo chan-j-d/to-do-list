01daeb7a28d96b71192e54344351637c86e64716
chan-j-d <chan.j.d.98@gmail.com>
2021-08-30 23:28:36 +0800

    Add logging for missing fields

--- src/main/java/storage/JacksonStorageImpl.java
+++ src/main/java/storage/JacksonStorageImpl.java
@@ -2,14 +2,21 @@ package storage;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import logging.LogsManager;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public abstract class JacksonStorageImpl<T> {
+
+    private static final Logger LOGGER = LogsManager.getLogger(JacksonStorageImpl.class);
+    private static final String MESSAGE_FIELD_NOT_FOUND = "At least one field was missing when loading.";
 
     private final ObjectMapper objectMapper;
 
@@ -31,7 +38,12 @@ public abstract class JacksonStorageImpl<T> {
     }
 
     public T load(Class<? extends JsonStorer<T>> classType, Path path) throws IOException {
-        JsonStorer<T> jsonStorer = objectMapper.readValue(Paths.get(path.toString()).toFile(), classType);
+        JsonStorer<T> jsonStorer = null;
+        try {
+            jsonStorer = objectMapper.readValue(Paths.get(path.toString()).toFile(), classType);
+        } catch (JsonMappingException jme) {
+            LOGGER.log(Level.INFO, MESSAGE_FIELD_NOT_FOUND, jme);
+        }
         return jsonStorer.toJavaType();
     }
 }
