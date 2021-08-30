0582606ceb4410a1cc9aa214f6d788c66de09c1d
chan-j-d <chan.j.d.98@gmail.com>
2021-08-31 00:58:08 +0800

    Fix checkstyle issues in src/main

--- src/main/java/storage/JacksonStorageImpl.java
+++ src/main/java/storage/JacksonStorageImpl.java
@@ -4,14 +4,13 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import logging.LogsManager;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import logging.LogsManager;
 
 public abstract class JacksonStorageImpl<T> {
 
@@ -25,6 +24,11 @@ public abstract class JacksonStorageImpl<T> {
         objectMapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
     }
 
+    /**
+     * Saves a {@code JsonStorer} object to the designated path.
+     * This method handles missing directory paths specified by the {@code path}.
+     * Throws {@code IOException} if it is unable to save the object.
+     */
     public boolean save(JsonStorer<T> storer, Path path) throws IOException {
         boolean doesFileExist = Files.exists(path);
         Path parentDirectory = path.getParent();
@@ -37,6 +41,12 @@ public abstract class JacksonStorageImpl<T> {
         return true;
     }
 
+    /**
+     * Loads the object specified by {@code path} into the provided {@code classType}.
+     * If there are missing fields in the save file compared to the {@code JsonStorer<T> type},
+     * a message is logged and the field is given a default value.
+     * Throws {@code IOException} if the load was unsuccessful for other reasons.
+     */
     public T load(Class<? extends JsonStorer<T>> classType, Path path) throws IOException {
         JsonStorer<T> jsonStorer = null;
         try {
