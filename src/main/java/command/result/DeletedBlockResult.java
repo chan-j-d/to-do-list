468ad9574651f0fcf30b110a26bf309b7b985f36
chan-j-d <chan.j.d.98@gmail.com>
2021-09-01 00:43:30 +0800

    Add command result classes implementation

--- src/main/java/command/result/DeletedBlockResult.java
+++ src/main/java/command/result/DeletedBlockResult.java
@@ -1,4 +1,20 @@
 package command.result;
 
-public class DeletedBlockResult {
+import io.IoInterface;
+import task.TaskList;
+
+public class DeletedBlockResult implements CommandResult {
+
+    private final int deletedIndex;
+    private final TaskList taskList;
+
+    public DeletedBlockResult(TaskList taskList, int deletedIndex) {
+        this.taskList = taskList;
+        this.deletedIndex = deletedIndex;
+    }
+
+    @Override
+    public void updateInterface(IoInterface ioInterface) {
+        ioInterface.removeBlock(taskList, deletedIndex);
+    }
 }
--- src/main/java/command/result/EditedBlocksResult.java
+++ src/main/java/command/result/EditedBlocksResult.java
@@ -1,11 +1,22 @@
 package command.result;
 
 import io.IoInterface;
+import task.TaskList;
 
 public class EditedBlocksResult implements CommandResult {
 
+    private final TaskList taskList;
+    private final int[] indicesToEdit;
+
+    public EditedBlocksResult(TaskList taskList, int... indices) {
+        this.taskList = taskList;
+        indicesToEdit = indices;
+    }
+
     @Override
     public void updateInterface(IoInterface ioInterface) {
-
+        for (int i : indicesToEdit) {
+            ioInterface.updateUserBlock(taskList, i);
+        }
     }
 }
