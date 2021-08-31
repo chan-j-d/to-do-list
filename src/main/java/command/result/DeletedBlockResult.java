c37a4bc36292116e2d6f6fdb1ea8fc6d8a15034e
chan-j-d <chan.j.d.98@gmail.com>
2021-09-01 00:19:00 +0800

    Add skeleton classes

---
+++ src/main/java/command/result/DeletedBlockResult.java
@@ -0,0 +1,4 @@
+package command.result;
+
+public class DeletedBlockResult {
+}
---
+++ src/main/java/command/result/EditedBlocksResult.java
@@ -0,0 +1,11 @@
+package command.result;
+
+import io.IoInterface;
+
+public class EditedBlocksResult implements CommandResult {
+
+    @Override
+    public void updateInterface(IoInterface ioInterface) {
+
+    }
+}
