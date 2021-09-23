57cf78773fb3f04eb6937b8a63c5ef74c3fe4d6e
chan-j-d <chan.j.d.98@gmail.com>
2021-09-24 00:22:34 +0800

    Remove println

--- src/test/java/command/AddBlockCommandTest.java
+++ src/test/java/command/AddBlockCommandTest.java
@@ -45,7 +45,6 @@ public class AddBlockCommandTest extends CommandTestTemplate {
 
         Assertions.assertThrows(CommandException.class, () -> invalidAddBlockCommand1.run(modelTaskList));
         Assertions.assertThrows(CommandException.class, () -> invalidAddBlockCommand2.run(modelTaskList));
-        System.out.println(modelTaskList);
     }
 
 }
